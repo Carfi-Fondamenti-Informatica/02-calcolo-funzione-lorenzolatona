88afda2e0ebc09e276045fc54e973f5d5e572880
lorenzolatona <115403824+lorenzolatona@users.noreply.github.com>
2022-11-15 11:33:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main() {
+   float a=0,b=0,x=0,y=0;
+    cin>>a>>b>>x>>y;
+    // Sia z il valore delle funzioni stampato a video
+    if (x<0&&y>0){
+        cout<<"z="<<(a*x-b*y)<<endl;
+    } else if (x>=0&&y<=0){
+        cout<<"z="<<(a*x*x-b*y)<<endl;
+    }else{
+        cout<<"z="<<(a*x+b*y*y)<<endl;
+    }
    return 0;
 }
